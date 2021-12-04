3c6615fb049c0ec69ae8145f665df6a5cd710821
jorge <91345902+jreyesgfi@users.noreply.github.com>
2021-12-04 14:03:52 +0100

    Iniciando con pandas

--- limpieza.py
+++ limpieza.py
@@ -1,7 +1,13 @@
 import pandas as pd
+from pandasql import sqldf
+
+
 
 pd.options.display.max_rows = 10
-df = pd.read_csv('../data_act_01.csv')
+df = pd.read_csv('../data_act_01.csv', sep = ';')
 
 
-print(df.to_string()) 
+print(df)
+print(df.query(' CrimeId ==  160912801 '))
+
+
