ba45f828a217c608489712a8967bd33b45ef7bc6
jorge <91345902+jreyesgfi@users.noreply.github.com>
2021-12-04 14:20:30 +0100

    Primeras condiciones

--- limpieza.py
+++ limpieza.py
@@ -5,9 +5,16 @@ from pandasql import sqldf
 
 pd.options.display.max_rows = 10
 df = pd.read_csv('../data_act_01.csv', sep = ';')
-
-
 print(df)
-print(df.query(' CrimeId ==  160912801 '))
 
 
+#print(df)
+#print(df.query(' CrimeId ==  160912801 '))
+
+tamañoParte = 10**3 // 2  # you may want to adjust it ... 
+for parte in pd.read_csv('../data_act_01.csv', chunksize=tamañoParte, sep= ';'):
+    pedazoToTest = parte.query(' CrimeId > 160932801 ')
+    pedazoToTest.to_csv('output.csv', mode='a', index=False)
+
+dff = pd.read_csv('output.csv', sep = ',')
+print(dff)
